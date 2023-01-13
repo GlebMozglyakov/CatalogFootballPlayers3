efb7b1786cab2acae98442783123e4632be83d27
GlebMozglyakov <mozglgleb2004@gmail.com>
2023-01-13 13:21:50 +0500

    Create model Footballer Create all fields

--- Models/Footballer.cs
+++ Models/Footballer.cs
@@ -1,9 +1,36 @@
-﻿namespace CatalogFootballPlayers3.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CatalogFootballPlayers3.Models
 {
     public class Footballer
     {
+        public enum Gender
+        {
+            Man,
+            Woman
+        }
+
+        public enum Countries
+        {
+            Russia,
+            USA,
+            Italy
+        }
+
+        [Key]
+        public int Id { get; set; }
+
         public string Name { get; set; }
 
         public string Surname { get; set; }
+
+        public Gender GenderFootballer { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string TeamName { get; set; }
+
+        public Countries Country { get; set; }
     }
 }
