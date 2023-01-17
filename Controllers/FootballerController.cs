df726aad1ec6b52810dd1ba4c24628935aadb9b8
GlebMozglyakov <mozglgleb2004@gmail.com>
2023-01-17 11:11:02 +0500

    Создан основной функционал

--- Controllers/FootballerController.cs
+++ Controllers/FootballerController.cs
@@ -38,7 +38,76 @@ namespace CatalogFootballPlayers3.Controllers
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
+
             return View(obj);
 		}
-	}
+
+		//GET - EDIT
+		public IActionResult Edit(int? id)
+		{
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Footballer.Find(id);
+
+            if (obj == null) 
+            {
+                return NotFound();
+            }
+
+			return View(obj);
+		}
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Footballer obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Footballer.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Footballer.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Footballer.Find(id);
+            
+            if (obj == null)
+            {
+                return NotFound();
+            }
+        
+            _db.Footballer.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
 }
